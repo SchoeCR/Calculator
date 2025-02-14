cea68322813ef7a355c4b239d0d260fd996f83d6
SchoeCR <159347003+SchoeCR@users.noreply.github.com>
2025-02-14 19:54:58 +1000

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declare variables and then initialise to zero
+int num1 = 0; int num2 = 0;
+
+// Display title as the C# console calculator app.
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+// Ask the user to type the first number.
+Console.WriteLine("Type a number, and then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type another number, and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to choose and option.
+Console.WriteLine("Choose and option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.WriteLine("Your option? ");
+
+// Use a switch statement to do the math.
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1)
+}
